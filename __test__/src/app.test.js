86985b3e0fa03ba49712466318e67148f4c63c32
FranciscoHellbusch <fghellbusch@gmail.com>
2023-01-28 21:42:19 -0300

    feature/ add basic setup configuration

---
+++ __test__/src/app.test.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+
+const {app} = require('../../src/app.init')
+
+describe('App initialization', () => {
+    let server
+    beforeAll(() => {
+        server = app.listen()
+    })
+
+    afterAll(() => {
+        server.close()
+    })
+
+    test('App port', async () => {
+        const port = process.env.PORT || 8080
+        expect(app.port).toBe(port)
+    })
+})
