6e538da14041f0ad58711b1ffdfebab52d0cc1f4
FranciscoHellbusch <fghellbusch@gmail.com>
2023-02-10 20:30:05 -0300

    feature/ connection created with mongoose

--- src/api/app.init.js
+++ src/api/app.init.js
@@ -5,8 +5,14 @@ const express = require('express')
 const HealthCheck = require('./controllers/healthCheck')
 const morgan = require('./middlewares/loggers/morgan')
 const errorHandler = require('./middlewares/errorHandler')
+const MongoConnect = require('./common/db/connect')
+const { logger } = require('./middlewares/loggers/winston')
 
 const createApp = () => {
+    MongoConnect()
+    .then(() => logger.info('MongoDB connection established'))
+    .catch((err) => logger.fatal(err))
+
     return new App({
         routes: [new HealthCheck('/health', 'api-health')],
         middlewares: [express.json(), morgan()],
---
+++ src/api/common/db/connect.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const configuration = require('../configuration')
+
+const MongoConnect = async () => {
+    let MONGO_URL = ''
+    const MONGO_DB_NAME = process.env.MONGO_DB_NAME
+    const MONGO_USER = process.env.MONGO_USER
+    const MONGO_PASSWORD = process.env.MONGO_PASSWORD
+    const MONGO_HOST = process.env.MONGO_HOST
+    const MONGO_QUERY = process.env.MONGO_QUERY
+
+    if (!MONGO_USER) {
+        MONGO_URL = `mongodb://${process.env.MONGO_URI}/${MONGO_DB_NAME}`
+    } else {
+        MONGO_URL = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_DB_NAME}?${MONGO_QUERY}`
+    }
+
+    await mongoose.set('strictQuery', false);
+
+    await mongoose.connect(MONGO_URL, configuration.mongo)
+}
+
+module.exports = MongoConnect
